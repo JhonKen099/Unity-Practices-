30f70a3ecdefdae1621e076fb47e76233041380c
JhonKen099 <125842622+JhonKen099@users.noreply.github.com>
2023-04-10 09:58:03 -0400

    Update 5: dash funcional (faltan animaciones)

--- My project/Assets/script/player/playerMovement.cs
+++ My project/Assets/script/player/playerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace Cainos.PixelArtTopDown_Basic
 {
-    public class TopDownCharacterController : MonoBehaviour
+    public class playerMovement : MonoBehaviour
     {
         [SerializeField] private int velocidad;
 
@@ -20,8 +20,8 @@ namespace Cainos.PixelArtTopDown_Basic
         [SerializeField] private float tiempoDash;
         [SerializeField] private float gravedadIni;
         [SerializeField] private bool puedeDash;
-
-
+        [SerializeField] private float posix;
+        [SerializeField] private float posiy;
 
         [SerializeField] private bool puedeMover;
 
@@ -36,10 +36,12 @@ namespace Cainos.PixelArtTopDown_Basic
 
             gravedadIni = rgbody.gravityScale;
 
-             veloDash = 3;
+             veloDash = 5;
              tiempoDash = 0.3f;
 
              playerAnimator = GetComponent<Animator>();
+
+             puedeMover = true;
         }
 
 
@@ -53,6 +55,7 @@ namespace Cainos.PixelArtTopDown_Basic
             playerAnimator.SetFloat("horizontal",direccion.x);
             playerAnimator.SetFloat("vertical",direccion.y);
             playerAnimator.SetFloat("Speed",direccion.sqrMagnitude);
+
             //Animacion De "Uy Quieto"
             if (Input.GetAxisRaw("Horizontal") != 0  
             || Input.GetAxisRaw("Vertical") != 0  || 
@@ -81,24 +84,32 @@ namespace Cainos.PixelArtTopDown_Basic
                     enfriDash = 0f;
                 }
             }
-            // Llamada funcion para dashear
-            if (Input.GetKey(KeyCode.C) && puedeDash == true)
+                        
+            if (Input.GetAxisRaw("Horizontal") != 0  
+                || Input.GetAxisRaw("Vertical") != 0  || 
+                Input.GetAxisRaw("Horizontal") != 0 && Input.GetAxisRaw("Vertical") != 0)
             {
-                StartCoroutine(Dash());
+                posix = Input.GetAxisRaw("Horizontal");
+                posiy = Input.GetAxisRaw("Vertical");
+                
             }
 
-            
-
+            // Llamada funcion para dashear
+            if (Input.GetKey(KeyCode.C) && puedeDash == true)
+                {   
+                        StartCoroutine(Dash(posix, posiy));
+                }
 
         }
 
         //Funcion Dash
-        private IEnumerator Dash()
+        private IEnumerator Dash(float velox, float veloy)
         {
             puedeMover = false;
             puedeDash = false;
             rgbody.gravityScale = 0;
-            rgbody.velocity = new Vector2(veloDash, 0);
+
+            rgbody.velocity = new Vector2(velox * veloDash, veloy * veloDash);
 
             yield return new WaitForSeconds(tiempoDash);
 
